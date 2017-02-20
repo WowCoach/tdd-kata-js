1107ff23a1608ef8aa5b2fa08cd0af2e59fc41ad
Waterstrong <sqlin@thoughtworks.com>
2017-02-20 22:18:50 +0800

    add test case to return BUZZ given number can be divided by 5

--- test/fizz-bizz.spec.js
+++ test/fizz-bizz.spec.js
@@ -5,11 +5,19 @@ describe('Test Fizz Buzz', () => {
     it('should return a given number as a string', () => {
         expect(fizzBuzz(1)).to.equal('1');
     });
+
     it('should return FIZZ given number can be divided by 3', () => {
         expect(fizzBuzz(3)).to.equal('FIZZ');
         expect(fizzBuzz(6)).to.equal('FIZZ');
         expect(fizzBuzz(9)).to.equal('FIZZ');
     });
+
+    it('should return BUZZ given number can be divided by 5', () => {
+        expect(fizzBuzz(5)).to.equal('BUZZ');
+        expect(fizzBuzz(10)).to.equal('BUZZ');
+        expect(fizzBuzz(25)).to.equal('BUZZ');
+    });
+
 });
 
 
