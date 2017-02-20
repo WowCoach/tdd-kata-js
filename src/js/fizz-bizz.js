fbec78183fef706857706325f21fa4ee511af5b3
Waterstrong <sqlin@thoughtworks.com>
2017-02-20 22:27:52 +0800

    refactor the third time to simplify the fizz buzz logic

--- src/js/fizz-bizz.js
+++ src/js/fizz-bizz.js
@@ -1,17 +1,12 @@
-const isFizz = (num) => {
-    return num % 3 === 0;
-};
-const isBuzz = (num) => {
-    return num % 5 === 0;
-};
 export const fizzBuzz = (num) => {
-    let result = '';
-    if (isFizz(num)) {
-        result += 'FIZZ';
-    }
-    if (isBuzz(num)) {
-        result += 'BUZZ';
-    }
-    return result || num.toString();
+    return fizz(num) + buzz(num) || num.toString();
+};
+
+const fizz = (num) => {
+    return num % 3 ? '' : 'FIZZ';
+};
+
+const buzz = (num) => {
+    return num % 5 ? '' : 'BUZZ';
 };
 
